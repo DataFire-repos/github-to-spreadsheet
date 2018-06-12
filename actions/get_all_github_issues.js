6ea1fe3c57454bd67f3df8d6c17fc880f5c878b2
DataFire <info@datafire.io>
2018-06-12 08:44:31 -0400

    create action for github issues

--- actions/get_all_github_issues.js
+++ actions/get_all_github_issues.js
@@ -8,21 +8,22 @@ module.exports = new datafire.Action({
     title: "repo"
   }, {
     type: "string",
-    title: "user"
+    title: "owner"
   }],
   handler: async (input, context) => {
     let curPage = 1;
     let allIssues = [];
     let lastIssues = null;
     while (!lastIssues || lastIssues.length) {
-      lastIssues = await github.user.repo.issues.get({
+      console.log(input);
+      lastIssues = await github.repos.owner.repo.issues.get({
         repo: input.repo,
-        user: input.user,
+        owner: input.owner,
         state: 'all',
         page: curPage++,
         sort: 'created',
         direction: 'desc',
-      });
+      }, context);
       allIssues = allIssues.concat(lastIssues);
     }
     return allIssues;
